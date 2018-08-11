6fa66e6c7e837f017cfb617c23ea32d3e08def45
lmy98129 <41624140@xs.ustb.edu.cn>
2018-08-11 22:12:04 +0800

    index tab-swiper-scroll built

--- pages/index/index.js
+++ pages/index/index.js
@@ -17,6 +17,21 @@ var recomArray = [
     "author": "我的老师",
     "content": "音悦智能教学辅助设备采用PCB板、FPC板以及无毒无害的电路 板印刷工艺，保证人体健康；其表面涂有环保漆，不容易被人手的汗液"
   },
+  {
+    "title": "吉他初学者必看",
+    "author": "易音科技小易",
+    "content": "音悦智能教学辅助设备采用PCB板、FPC板以及无毒无害的电路 板印刷工艺，保证人体健康；其表面涂有环保漆，不容易被人手的汗液"
+  },
+  {
+    "title": "学吉他难坚持怎么办？",
+    "author": "吉他红人张佳铂",
+    "content": "音悦智能教学辅助设备采用PCB板、FPC板以及无毒无害的电路 板印刷工艺，保证人体健康；其表面涂有环保漆，不容易被人手的汗液"
+  },
+  {
+    "title": "10首今年最火吉他谱",
+    "author": "我的老师",
+    "content": "音悦智能教学辅助设备采用PCB板、FPC板以及无毒无害的电路 板印刷工艺，保证人体健康；其表面涂有环保漆，不容易被人手的汗液"
+  },
 ];
 
 Page({
@@ -24,10 +39,21 @@ Page({
     isIpx: false,
     isIp4: false,
     tabbarActiveIndex: 0,
-    msgArray: []
+    currentTab: 0,
+    msgArray: [],
+    winHeight: 0,
+    scrollHeight: 0
   },
   //事件处理函数
   onLoad: function () {
+    let that = this;
+    wx.getSystemInfo({
+      success: function(res) {
+        that.setData({
+          winHeight: res.windowHeight
+        })
+      },
+    })
     // if (app.globalData.userInfo) {
     //   this.setData({
     //     userInfo: app.globalData.userInfo,
@@ -56,8 +82,10 @@ Page({
     // }
   },
   onShow() {
+    let length = recomArray.length
     this.setData({
-      msgArray: recomArray
+      msgArray: recomArray,
+      scrollHeight: length*480
     })
     // wx.hideTabBar();
     // wx.navigateTo({
@@ -77,5 +105,31 @@ Page({
     wx.navigateTo({
       url: '../bluetooth/bluetooth',
     })
+  },
+
+  bindTab(e) {
+    if (this.data.currentTab === e.currentTarget.dataset.index) {
+      return;
+    } else {
+      let arr = this.data.msgArray
+      let length = arr.length
+      this.setData({
+        currentTab: e.currentTarget.dataset.index,
+        scrollHeight: length*300
+      });
+    }
+  },
+
+  bindSwiper(e) {
+    this.setData({
+      currentTab: e.detail.current
+    })
+  },
+
+  refresh() {
+    wx.showToast({
+      title: '刷新成功',
+    })
   }
+
 })
